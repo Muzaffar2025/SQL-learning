2fee2c2ec9f999c8bd6dd2fbda508fe691a6ab90
Muzaffar2025 <muzaffar.abdurakhmonow@gmail.com>
2025-05-22 13:21:28 +0500

    Update lesson1.sql

--- lesson-1/homework/lesson1.sql
+++ lesson-1/homework/lesson1.sql
@@ -1,1 +1,8 @@
-create database 
+--5-vazifa
+create database SchoolDB
+CREATE TABLE Students (StudentID INT PRIMARY KEY, Name VARCHAR(50), Age INT)
+select * from Students
+
+--9-vazifa
+--file yuklab olinadi C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL backup filega tashlanadi, Databasesdan restore database tanlanadi
+devicedan ... nuqta bosiladi va fayl tanlab yuklab olinadi
